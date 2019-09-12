6f93cc1aa65c3aaf9b4a62881a7a695c9b217ac6
Karla <kknf.eng@uea.edu.br>
2019-09-11 21:13:30 -0300

    Excluindo e Armazenando atividades

--- teste/to_do.js
+++ teste/to_do.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var to_dos = [
-    'Estudar Javascript',
-    'Evoluir profissionalmente',
-    'Colocar comida pro Gordinho'
-];
+var to_dos = JSON.parse(localStorage.getItem('list_to_dos')) || [];
 
 function renderTo_do() {
     listElement.innerHTML = '';
@@ -14,7 +10,21 @@ function renderTo_do() {
         var to_doElement = document.createElement('li');
         var to_doText = document.createTextNode(to_do);
 
+        var linkElement = document.createElement('a');
+
+        linkElement .setAttribute('href', '#');
+
+        var pos = to_dos.indexOf(to_do);
+        
+        linkElement.setAttribute('onclick', 'deleteTo_do('+ pos +')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+
         to_doElement.appendChild(to_doText);
+        to_doElement.appendChild(linkElement);
         listElement.appendChild(to_doElement);
 
     }
@@ -28,6 +38,18 @@ function addTo_do() {
     to_dos.push(todoText);
     inputElement.value = '';
     renderTo_do();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTo_do;
+
+function deleteTo_do(pos){
+    to_dos.splice(pos, 1);
+    renderTo_do();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    
+    localStorage.setItem('list_to_dos', JSON.stringify(to_dos));
+}
